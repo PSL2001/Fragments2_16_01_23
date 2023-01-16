912db0cc3cb1a3389bbb72fa71c9885f58cdb221
Pablo Sánchez López <65461723+PSL2001@users.noreply.github.com>
2023-01-16 13:59:39 +0100

    Creado layouts y 2do activity

---
+++ app/src/main/java/com/example/fragment2_16_01_23/DosActivity.kt
@@ -0,0 +1,11 @@
+package com.example.fragment2_16_01_23
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DosActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dos)
+    }
+}
