271a22be337abe2d2692a7091ea989e4f2de3525
Pablo Sánchez López <65461723+PSL2001@users.noreply.github.com>
2023-01-16 14:20:59 +0100

    Creado interfaz y modificado layouts

--- app/src/main/java/com/example/fragment2_16_01_23/MainActivity.kt
+++ app/src/main/java/com/example/fragment2_16_01_23/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.fragment2_16_01_23
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.fragment2_16_01_23.fragments.InterfazMenuClick
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), InterfazMenuClick {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onMenuClick(nBtn: Int) {
+        val i = Intent(this, DosActivity::class.java).apply {
+            putExtra("Boton", nBtn)
+        }
+        startActivity(i)
     }
 }
---
+++ app/src/main/java/com/example/fragment2_16_01_23/fragments/InterfazMenuClick.kt
@@ -0,0 +1,5 @@
+package com.example.fragment2_16_01_23.fragments
+
+interface InterfazMenuClick {
+    fun onMenuClick(nBtn: Int)
+}
